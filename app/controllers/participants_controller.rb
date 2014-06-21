dd83dfea0bca18742f22f75f7449e855d41d9ca2
Daniel Deutsch <danielallendeutsch@gmail.com>
2014-06-21 15:22:19 -0500

    Starting Huroku

---
+++ app/controllers/participants_controller.rb
@@ -0,0 +1,24 @@
+class ParticipantsController < ApplicationController
+	def create
+    @event = Event.find(params[:event_id])
+    @participant = @event.participants.new(event_id_params)
+    if @participant.save
+      redirect_to event_url(@event)
+    else
+      redirect_to event_url(@event)
+    end
+end
+
+def add_guest
+    @event = Event.find(params[:event_id])
+    @participant = Participant.find(params[:id])
+    @participant.guests += 1
+    @participant.save
+    redirect_to event_url(@event)
+  end
+
+private
+def event_id_params
+	params.require(:participant).permit(:name, :guests)
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  resources :events
+
+  resources :events  do
+    resources :participants do
+      member do
+        post :add_guest
+      end
+    end
+  end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
